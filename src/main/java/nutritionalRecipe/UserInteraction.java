b965dd477881a814ebce49a7c1dcfede747010b2
juanfranciscog <juangole@seas.upenn.edu>
2020-01-05 16:49:36 -0500

    Update UserInteraction.java
    Added a few lines of creteRecipe method

--- src/main/java/nutritionalRecipe/UserInteraction.java
+++ src/main/java/nutritionalRecipe/UserInteraction.java
@@ -1,11 +1,21 @@
 package nutritionalRecipe;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class UserInteraction {
     private String recipeName;
-    private String ingredient;
+    private HashMap <String, Integer> ingredients = new HashMap<String, Integer>();
     private double ingredientQuantity;
     private int numberOfPortions;
     private double guessedCalories;
     private double realCalories;
 
+    public void createRecipe() {
+        Scanner in = new Scanner();
+        System.out.println("Please enter the name of the recipe");
+        recipeName = in.nextLine();
+        System.out.println("Now let's enter the ingredients and the quantities. When done, please type END");
+
+    }
 }
