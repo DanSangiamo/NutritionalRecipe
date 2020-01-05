59235f43fecba1a56ea00c01fd03299866d222ac
juanfranciscog <juangole@seas.upenn.edu>
2020-01-05 14:54:50 -0500

    Create UserInteraction.java
    Initial creation of UserInteraction

---
+++ src/main/java/nutritionalRecipe/UserInteraction.java
@@ -0,0 +1,11 @@
+package nutritionalRecipe;
+
+public class UserInteraction {
+    private String recipeName;
+    private String ingredient;
+    private double ingredientQuantity;
+    private int numberOfPortions;
+    private double guessedCalories;
+    private double realCalories;
+
+}
