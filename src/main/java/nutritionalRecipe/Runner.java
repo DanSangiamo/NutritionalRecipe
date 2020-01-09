8f27c641cd75f3429b5efb2ca4cb62f31a0200bd
juanfranciscog <juangole@seas.upenn.edu>
2020-01-08 21:36:54 -0500

    Update Runner.java
    Code working before adding ingredient guess

--- src/main/java/nutritionalRecipe/Runner.java
+++ src/main/java/nutritionalRecipe/Runner.java
@@ -1,9 +1,22 @@
 package nutritionalRecipe;
 
+import java.io.IOException;
+
 public class Runner {
     public static void main(String[] args) {
 
         UserInteraction ui = new UserInteraction();
-        ui.guessCalories(ui.createRecipe());
+        Recipe recipe = ui.createRecipe();
+        ui.guessCalories(recipe);
+        
+        RecipeBox rb = new RecipeBox();
+        try {
+	    rb.createDatabaseFile();
+	} catch (IOException e) {
+
+	    e.printStackTrace();
+	}
+        rb.storeRecipes(recipe);
+        System.out.println("We have saved the recipe in a file \"UserRecipes.csv\"");
     }
 }
