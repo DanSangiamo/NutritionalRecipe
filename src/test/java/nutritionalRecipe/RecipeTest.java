1d6c71c88846618debd25048a8bfb107fac94bb0
mbaumgarten <marthabaumgarten129@gmail.com>
2020-01-12 10:05:59 -0500

    recipe test again

--- src/test/java/nutritionalRecipe/RecipeTest.java
+++ src/test/java/nutritionalRecipe/RecipeTest.java
@@ -16,7 +16,19 @@ class RecipeTest {
         Ingredient ing2= new Ingredient("sugar", 1, "Cup");
         ing1.setCalories(100);
         ing2.setCalories(60);
+        ing1.setFat(10);
+        ing2.setFat(0);
+        ing1.setCarbs(15);
+        ing2.setCarbs(20);
+        ing1.setSugar(10);
+        ing2.setSugar(20);
+        ing1.setFiber(2);
+        ing2.setFiber(0);
+        ing1.setProtein(2);
+        ing2.setProtein(0);
         myRecipe.setPortions(4);
+        ingredients.add(ing1);
+        ingredients.add(ing2);
         assertEquals(myRecipe.getNutritionFacts().get(0), 40);
     }
 
